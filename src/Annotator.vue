586528945d94a2cbe25e507f57f6813c59947660
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2017-12-23 06:57:07 +0700

    Add properties and fix circle bug - add prop for setting grid (snapping) - add prop for enabling inertia - add prop for setting minimum size of annotation - fix bug(edge resize) for limit resizing when circle at the end of the canvas_svg

--- src/Annotator.vue
+++ src/Annotator.vue
@@ -5,7 +5,7 @@
         <slot></slot>
       </div>
     </foreignObject>
-    <g class="foreground">
+    <g ref="fg" class="foreground">
       <slot name="annotation"></slot>
     </g>
   </svg>
@@ -19,14 +19,15 @@ export default {
   props: {
     width: Number,
     height: Number,
-    minAnonSize: {
-      type: Array,
-      default: function () {
-        return [10, 10]
-      },
-      validator: function (value) {
-        return value.length == 2 && value.every(v => typeof (v) === 'number')
-      }
+    inertia: Boolean,
+    grid: {
+      type: [Array, Number],
+      validator: (value) => (value.length == 2 && value.every(v => typeof (v) === 'number')) || (typeof (value) === 'number')
+    },
+    minSize: {
+      type: [Array, Number],
+      default: () => 10,
+      validator: (value) => (value.length == 2 && value.every(v => typeof (v) === 'number')) || (typeof (value) === 'number')
     },
   },
 
@@ -38,89 +39,103 @@ export default {
   },
 
   mounted () {
-    // const svg = SVG(this.$refs.svg)
+    const master = SVG(this.$refs.svg)
     this.w = this.$refs.bg.scrollWidth
     this.h = this.$refs.bg.scrollHeight
 
-    for (const i in this.$slots.annotation) {
-      const el = this.$slots.annotation[i].elm
-      if (el) {
-        interact(el)
-          .draggable({
-            inertia: true,
-            // keep the element within the area of it's parent
-            restrict: {
-              restriction: 'svg',
-              elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-            },
-            autoScroll: true,
+    let gridTarget = this.grid ? interact.createSnapGrid({
+      x: this.grid[0],
+      y: this.grid[1],
+    }) : null
 
-            // call this function on every dragmove event
-            onmove: event => SVG.adopt(event.target).dmove(event.dx, event.dy)
-          })
-          .resizable({
-            // resize from all edges and corners
-            edges: { left: true, right: true, bottom: true, top: true },
+    let [minWidth, minHeight] = typeof (this.minSize) === 'number' ? [this.minSize, this.minSize] : this.minSize
 
-            // keep the edges inside the parent
-            restrictEdges: {
-              outer: 'svg',
-            },
+    interact('*', { context: this.$refs.foreground })
 
-            // minimum size
-            restrictSize: {
-              min: { width: this.minAnonSize[0], height: this.minAnonSize[1] },
-            },
-            inertia: true,
+      .draggable({
+        inertia: this.inertia,
+        snap: { targets: [gridTarget] },
 
-            // call this function on every resizemove event
-            onmove: (event) => {
-              const target = SVG.adopt(event.target)
+        // keep the element within the area of it's parent
+        restrict: {
+          restriction: 'svg',
+          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+        },
+        autoScroll: true,
 
-              let x = target.x() + event.deltaRect.left
-              let y = target.y() + event.deltaRect.top
-              switch (target.type) {
-                case 'ellipse':
-                  target.move(x + event.deltaRect.width / 2, y + event.deltaRect.height / 2)
-                  target.size(event.rect.width, event.rect.height)
-                  break;
-                case 'circle':
-                  const antiPhytagoras = (x, y) => x != 0 && y != 0 && Math.sign(x) != Math.sign(y) ? 0 : Math.sign(x + y) * Math.hypot(event.deltaRect.width, event.deltaRect.height)
-                  const delta = antiPhytagoras(event.deltaRect.width, event.deltaRect.height)
-                  const d = s => delta != 0 ? (s / 2) : 0
-                  const threshold = this.minAnonSize.reduce((sum, val) => sum + val) / 2 // because deltaRect will keep going when resizing in diagonal
+        // call this function on every dragmove event
+        onmove: event => SVG.adopt(event.target).dmove(event.dx, event.dy)
+      })
 
-                  if (event.rect.width > threshold && event.rect.height > threshold) {
-                    x += d(event.deltaRect.width)
-                    y += d(event.deltaRect.height)
-                  } else {
-                    x = target.x()
-                    y = target.y()
-                  }
+      .resizable({
+        inertia: this.inertia,
+        snap: { targets: [gridTarget] },
+        // resize from all edges and corners
+        edges: { left: true, right: true, bottom: true, top: true },
 
-                  let diameter = target.attr('r') * 2 + delta
-                  diameter = Math.max(diameter, threshold)
+        // keep the edges inside the parent
+        restrictEdges: {
+          outer: 'svg',
+        },
+        autoScroll: true,
 
-                  target.move(x, y)
-                  target.size(diameter)
+        // minimum size
+        restrictSize: {
+          min: { width: minWidth, height: minHeight },
+        },
 
-                  break;
-                default:
-                  target.move(x, y)
-                  target.size(event.rect.width, event.rect.height)
-                  break;
+        // call this function on every resizemove event
+        onmove: (event) => {
+          const target = SVG.adopt(event.target)
+
+          let x = target.x() + event.deltaRect.left
+          let y = target.y() + event.deltaRect.top
+          switch (target.type) {
+            case 'ellipse':
+              target.move(x + event.deltaRect.width / 2, y + event.deltaRect.height / 2)
+              target.size(event.rect.width, event.rect.height)
+              break;
+            case 'circle':
+              const antiPhytagoras = (x, y) => x != 0 && y != 0 && Math.sign(x) != Math.sign(y) ? 0 : Math.sign(x + y) * Math.hypot(event.deltaRect.width, event.deltaRect.height)
+              const delta = antiPhytagoras(event.deltaRect.width, event.deltaRect.height)
+              const d = s => delta != 0 ? (s / 2) : 0
+              const threshold = (minWidth + minHeight) / 2 // because deltaRect will keep going when resizing in diagonal
+
+              // BUG: still have slightly when moving diagonally
+              if (event.rect.width > threshold && event.rect.height > threshold) {
+                x += d(event.deltaRect.width)
+                y += d(event.deltaRect.height)
+              } else {
+                x = target.x()
+                y = target.y()
               }
-            }
-          })
 
-        /** Resize circle on rotation mode
-        @example
-        let r = target.attr('r') + (event.deltaRect.width || event.deltaRect.height)
-        target.radius(r)
-        */
-        // TODO(rotate): https://codepen.io/drsensor/pen/Vyjaao
-      }
-    }
+              let diameter = target.attr('r') * 2 + delta
+              diameter = Math.max(diameter, threshold)
+
+              // Enable resizing when circle in edge of canvas
+              const notEdgeTopLeft = x + Math.abs(event.deltaRect.width) >= 0 && y + Math.abs(event.deltaRect.height) >= 0
+              const notEdgeBottomRight = (x + diameter) <= master.width() + Math.abs(event.deltaRect.width) && (y + diameter) <= master.height() + Math.abs(event.deltaRect.height)
+
+              if (notEdgeBottomRight && notEdgeTopLeft) {
+                target.move(x, y)
+                target.size(diameter)
+              }
+              break;
+            default:
+              target.move(x, y)
+              target.size(event.rect.width, event.rect.height)
+              break;
+          }
+        }
+      })
+
+    /** Resize circle on rotation mode
+    @example
+    let r = target.attr('r') + (event.deltaRect.width || event.deltaRect.height)
+    target.radius(r)
+    */
+    // TODO(rotate): https://codepen.io/drsensor/pen/Vyjaao
   }
 
 }
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="annotation">
-    <annotations>
+    <annotations :minSize="50">
       <rect class="box" slot="annotation" x="10" y="10" width="100" height="100" />
       <rect class="box" slot="annotation" x="150" y="150" width="50" height="50" />
       <ellipse class="box" slot="annotation" cx="150" cy="150" rx="50" ry="100" />
-      <circle class="box" slot="annotation" cx="150" cy="150" r="50" />
+      <circle class="box" slot="annotation" cx="150" cy="150" r="100" />
       <polygon class="box" slot="annotation" points="200,10 250,190 160,210" />
       <path class="box" slot="annotation" d="M150 0 L75 200 L225 200 Z" />
       <img draggable="false" src="https://docs.microsoft.com/en-us/sql/reporting-services/media/ssrs-web-portal.png" />
