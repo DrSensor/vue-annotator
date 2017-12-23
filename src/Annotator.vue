f2832924fee4213c569f1402b973919731928b15
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2017-12-23 13:43:59 +0700

    Fix bug svg size not set properly when first loaded - update `w,h` at 23 fps until `src` of media tag (e.g <img>) finish downloading - fix cursor bug https://github.com/taye/interact.js/issues/536

--- src/Annotator.vue
+++ src/Annotator.vue
@@ -38,10 +38,19 @@ export default {
     }
   },
 
+  updated () {
+    this.w = this.width || this.$refs.bg.scrollWidth
+    this.h = this.width || this.$refs.bg.scrollHeight
+  },
+
   mounted () {
     const master = SVG(this.$refs.svg)
-    this.w = this.$refs.bg.scrollWidth
-    this.h = this.$refs.bg.scrollHeight
+    const media = this.$slots.default.filter(el => ['img', 'video', 'audio', 'picture'].includes(el.tag))
+    const interval = setInterval(() => {
+      let loadComplete = media.every(el => el.elm.complete)
+      this.$forceUpdate()
+      if (loadComplete) clearInterval(interval)
+    }, 43.48)
 
     let gridTarget = this.grid ? interact.createSnapGrid({
       x: this.grid[0],
@@ -50,85 +59,85 @@ export default {
 
     let [minWidth, minHeight] = typeof (this.minSize) === 'number' ? [this.minSize, this.minSize] : this.minSize
 
-    interact('*', { context: this.$refs.foreground })
+    for (const el of this.$slots.annotation)
+      interact(el.elm)
+        .draggable({
+          inertia: this.inertia,
+          snap: { targets: [gridTarget] },
 
-      .draggable({
-        inertia: this.inertia,
-        snap: { targets: [gridTarget] },
+          // keep the element within the area of it's parent
+          restrict: {
+            restriction: 'svg',
+            elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+          },
+          autoScroll: true,
 
-        // keep the element within the area of it's parent
-        restrict: {
-          restriction: 'svg',
-          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-        },
-        autoScroll: true,
+          // call this function on every dragmove event
+          onmove: event => SVG.adopt(event.target).dmove(event.dx, event.dy)
+        })
 
-        // call this function on every dragmove event
-        onmove: event => SVG.adopt(event.target).dmove(event.dx, event.dy)
-      })
+        .resizable({
+          inertia: this.inertia,
+          snap: { targets: [gridTarget] },
+          // resize from all edges and corners
+          edges: { left: true, right: true, bottom: true, top: true },
 
-      .resizable({
-        inertia: this.inertia,
-        snap: { targets: [gridTarget] },
-        // resize from all edges and corners
-        edges: { left: true, right: true, bottom: true, top: true },
+          // keep the edges inside the parent
+          restrictEdges: {
+            outer: 'svg',
+          },
+          autoScroll: true,
 
-        // keep the edges inside the parent
-        restrictEdges: {
-          outer: 'svg',
-        },
-        autoScroll: true,
+          // minimum size
+          restrictSize: {
+            min: { width: minWidth, height: minHeight },
+          },
 
-        // minimum size
-        restrictSize: {
-          min: { width: minWidth, height: minHeight },
-        },
+          // call this function on every resizemove event
+          onmove: (event) => {
+            const target = SVG.adopt(event.target)
 
-        // call this function on every resizemove event
-        onmove: (event) => {
-          const target = SVG.adopt(event.target)
+            let x = target.x() + event.deltaRect.left
+            let y = target.y() + event.deltaRect.top
+            switch (target.type) {
+              case 'ellipse':
+                target.move(x + event.deltaRect.width / 2, y + event.deltaRect.height / 2)
+                target.size(event.rect.width, event.rect.height)
+                break;
+              case 'circle':
+                const antiPhytagoras = (x, y) => x != 0 && y != 0 && Math.sign(x) != Math.sign(y) ? 0 : Math.sign(x + y) * Math.hypot(event.deltaRect.width, event.deltaRect.height)
+                const delta = antiPhytagoras(event.deltaRect.width, event.deltaRect.height)
+                const d = s => delta != 0 ? (s / 2) : 0
+                const threshold = (minWidth + minHeight) / 2 // because deltaRect will keep going when resizing in diagonal
 
-          let x = target.x() + event.deltaRect.left
-          let y = target.y() + event.deltaRect.top
-          switch (target.type) {
-            case 'ellipse':
-              target.move(x + event.deltaRect.width / 2, y + event.deltaRect.height / 2)
-              target.size(event.rect.width, event.rect.height)
-              break;
-            case 'circle':
-              const antiPhytagoras = (x, y) => x != 0 && y != 0 && Math.sign(x) != Math.sign(y) ? 0 : Math.sign(x + y) * Math.hypot(event.deltaRect.width, event.deltaRect.height)
-              const delta = antiPhytagoras(event.deltaRect.width, event.deltaRect.height)
-              const d = s => delta != 0 ? (s / 2) : 0
-              const threshold = (minWidth + minHeight) / 2 // because deltaRect will keep going when resizing in diagonal
+                // BUG: still have slight move when resizing diagonally
+                if (event.rect.width > threshold && event.rect.height > threshold) {
+                  x += d(event.deltaRect.width)
+                  y += d(event.deltaRect.height)
+                } else {
+                  x = target.x()
+                  y = target.y()
+                }
 
-              // BUG: still have slightly when moving diagonally
-              if (event.rect.width > threshold && event.rect.height > threshold) {
-                x += d(event.deltaRect.width)
-                y += d(event.deltaRect.height)
-              } else {
-                x = target.x()
-                y = target.y()
-              }
+                let diameter = target.attr('r') * 2 + delta
+                diameter = Math.max(diameter, threshold)
 
-              let diameter = target.attr('r') * 2 + delta
-              diameter = Math.max(diameter, threshold)
+                // Enable resizing when circle in edge of canvas
+                const notEdgeTopLeft = x + Math.abs(event.deltaRect.width) >= 0 && y + Math.abs(event.deltaRect.height) >= 0
+                const notEdgeBottomRight = (x + diameter) <= master.width() + Math.abs(event.deltaRect.width) && (y + diameter) <= master.height() + Math.abs(event.deltaRect.height)
 
-              // Enable resizing when circle in edge of canvas
-              const notEdgeTopLeft = x + Math.abs(event.deltaRect.width) >= 0 && y + Math.abs(event.deltaRect.height) >= 0
-              const notEdgeBottomRight = (x + diameter) <= master.width() + Math.abs(event.deltaRect.width) && (y + diameter) <= master.height() + Math.abs(event.deltaRect.height)
-
-              if (notEdgeBottomRight && notEdgeTopLeft) {
+                if (notEdgeBottomRight && notEdgeTopLeft) {
+                  target.move(x, y)
+                  target.size(diameter)
+                }
+                break;
+              default:
                 target.move(x, y)
-                target.size(diameter)
-              }
-              break;
-            default:
-              target.move(x, y)
-              target.size(event.rect.width, event.rect.height)
-              break;
+                target.size(event.rect.width, event.rect.height)
+                break;
+            }
           }
-        }
-      })
+        })
 
     /** Resize circle on rotation mode
     @example
@@ -137,15 +146,8 @@ export default {
     */
     // TODO(rotate): https://codepen.io/drsensor/pen/Vyjaao
   }
-
 }
 </script>
-
-
-<style>
-
-</style>
-
 
 <style scoped>
 .foreground {
