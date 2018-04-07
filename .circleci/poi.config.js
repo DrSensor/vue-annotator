d96456911c7cf5fa6199690229c2c1e5ef34d81d
Fahmi Akbar Wildana <fahmi.akbar.w@mail.ugm.ac.id>
2018-04-07 15:38:13 +0700

    0.11.4
    Fix missing metadata :p
    
    Fix build error when buidling storybook page
    
    Fix missing type declaration file when publishing

---
+++ .circleci/poi.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  filename: {
+    js: 'index.js',
+    css: 'index.css',
+    images: 'assets/images/[name].[ext]',
+    fonts: 'assets/fonts/[name].[ext]',
+    chunk: '[id].chunk.js'
+  }
+}
